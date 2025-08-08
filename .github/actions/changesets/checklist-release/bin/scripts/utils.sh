a0f0d1fc605c4c975edafee19129e2bc22e2277c
v0xvigneshwaran <vigneshwaran@vezham.com>
2025-08-09 05:25:46 +0530

    ci: testing

--- .github/actions/changesets/checklist-release/bin/scripts/utils.sh
+++ .github/actions/changesets/checklist-release/bin/scripts/utils.sh
@@ -8,8 +8,8 @@ check_dependencies() {
     fi
 
     # check NPM authentication...
-    log_info "check NPM auth..."
-    
+    log_info "check NPM auth... [vx-101]"
+
     if ! npm whoami &> /dev/null; then
         log_error "Not logged into npm. Please run 'npm login' first."
         exit 1
