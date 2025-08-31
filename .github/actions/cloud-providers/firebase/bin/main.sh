bb4b19f1308a5f9066bb3d18b72582ebd280671f
v0xvigneshwaran <vigneshwaran@vezham.com>
2025-08-31 07:09:17 +0530

    chore: update CI workflow and Firebase action scripts
    Refactored the Firebase main.sh script to remove redundant commands and clarify variable usage. Updated gen-build action to use VX_APP_NAME instead of NX_APP_NAME. Modified playground.helper.yml workflow to adjust environment variables, step ordering, and add nx/AI fix step for improved CI reliability.

--- .github/actions/cloud-providers/firebase/bin/main.sh
+++ .github/actions/cloud-providers/firebase/bin/main.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-# $name (accounts, business, hq,..)
-# $dir (path to ROOT_DIR from Working Dir)
+# $name (apps_pods/accounts, apps_pods/business, apps_suits/hq,..)
 # $1 - name (vx/config/firebase/$name.json)
-# $2 - dir (../../v)
 
 name=${1:-"app"}
-dir=${2:-"../.."}
-
-pwd
-ls
 
 # read config
 config=`cat vx/config/firebase/$name.json`
 
-pwd
-ls
-
 # create firebase.json file
 touch "firebase.json"
 
-pwd
-ls
-
 # write config to firebase.json
 echo $config >> firebase.json
-
-pwd
-ls
